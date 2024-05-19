af83abfcbf3c3a3791a2d2be25db877cc3b5287a
Pablo <pablo.compte.jeux@gmail.com>
2024-05-19 20:33:16 +0200

    FINALE VERSION I HOPE (2)

--- column.h
+++ column.h
@@ -18,21 +18,6 @@ struct column
 /*-Definition of type COLUMN*/
 typedef struct column COLUMN;
 /*-Definition of type dataframe-*/
-enum enum_type
-{
-    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
-};
-typedef enum enum_type ENUM_TYPE;
-union column_type{
-    unsigned int uint_value;
-    signed int int_value;
-    char char_value;
-    float float_value;
-    double double_value;
-    char* string_value;
-    void* struct_value;
-};
-typedef union column_type COL_TYPE ;
 
 typedef COLUMN* dataframe;
 COLUMN* create_col(char name[]);
